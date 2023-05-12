a1bd8e079b4498c8125aa38cab24b3b8045693f8
AungKhantZin <viennghink@outlook.com>
2023-05-12 18:53:22 +0630

    api

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 import tornado.web
 import tornado.ioloop
-
+import json
 class basicRequestHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.write("Some Changes")
+
+class listAPIRequestHandler(tornado.web.RequestHandler):
+	def get(self):
+		fh = open('list.txt', "r")
+		lists = fh.read().splitlines()
+		fh.close()
+		self.write(json.dumps(lists))
+		
 class listRequestHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render("index.html")
 
+class queryRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        num = self.get_argument("num")
+        if num.isdigit():
+            r = "odd" if int(num) % 2 else "even"
+            self.write(f"The integer {num} is {r}")
+        else:
+            self.write(f"{num} is not a valid integer.")
+
+class resourceParamRequestHandler(tornado.web.RequestHandler):
+	def get(self,studentName, courseId):
+		self.write(f"Welcome {studentName}  the course you are viewing is {courseId}")
+
 if __name__ == '__main__':
 	app = tornado.web.Application([
 		(r"/", basicRequestHandler),
-		(r"/animal",listRequestHandler)
+		(r"/api", listAPIRequestHandler),
+		(r"/animal",listRequestHandler),
+        (r"/isEven", queryRequestHandler),
+        (r"/students/([a-z]+)/([0-9]+)", resourceParamRequestHandler)
 		])
 
 	port = 8882
